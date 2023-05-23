522897df3b34a9301be335039bd3376e83318baa
kero13 <kero13@riversense.tw>
2023-05-23 11:01:34 +0800

    feat: conf and clean

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style']
+      }
+    ],
+    'vue/singleline-html-element-content-newline': 'off', // 這條開啟會限制 html 標籤有內容就換行，如果很短實在沒必要特別換行
+    'vue/require-default-prop': 'off', // 要求 props 要有預設值
+    'vue/multi-word-component-names': 'off', // Nuxt3 有一些元件都會是單一名稱，例如404
+    'vue/no-unused-components': 'warn'
   }
 }
